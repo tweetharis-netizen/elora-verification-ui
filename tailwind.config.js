be7be095bab927811f06b4cea96302146bb96ae6
tweetharis-netizen <tweetharis@gmail.com>
2025-12-22 17:17:24 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+// 📁 File: tailwind.config.js
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        elora: '#6c63ff'
+      },
+    },
   },
   plugins: [],
-}
+};

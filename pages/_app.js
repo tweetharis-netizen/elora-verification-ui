ff6bc1cd5b9b9e189567d7507b1080e2b859d615
tweetharis-netizen <tweetharis@gmail.com>
2025-12-23 22:02:56 +0800

    Fix build: remove firebase from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,5 @@
 import "../styles/globals.css";
-import { useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../lib/firebase";
 
-export default function App({ Component, pageProps }) {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
-      setUser(firebaseUser);
-      setLoading(false);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  if (loading) {
-    return null; // avoids flicker
-  }
-
-  return <Component {...pageProps} user={user} />;
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }

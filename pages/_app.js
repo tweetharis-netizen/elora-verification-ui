f17a6f3091027040ff9541a46bf9b02a5e5b22fb
tweetharis-netizen <tweetharis@gmail.com>
2025-12-23 20:39:58 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,24 @@
 import "../styles/globals.css";
-import Head from "next/head";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../lib/firebase";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return null; // avoids flicker
+  }
+
+  return <Component {...pageProps} user={user} />;
 }

f8eb0e40bff5629ca0e60dc10ab02cf7ccb32ea2
tweetharis-netizen <tweetharis@gmail.com>
2025-12-27 17:12:22 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Prevent deployments from failing due to ESLint rules.
-  // We’ll keep code quality high by fixing lint issues manually later,
-  // but deploy must not be blocked.
   eslint: {
+    // Prevent Vercel deploy failures from lint rules.
     ignoreDuringBuilds: true,
   },
 };

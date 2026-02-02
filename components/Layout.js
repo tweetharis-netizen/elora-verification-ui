fc245b51edcd57f55a30c2de764a13f14c6a9096
tweetharis-netizen <tweetharis@gmail.com>
2026-02-02 21:55:28 +0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,6 @@
-import Navbar from "./Navbar";
-
 export default function Layout({ children }) {
   return (
     <div className="elora-shell">
-      <Navbar />
       <main className="elora-main">{children}</main>
     </div>
   );
